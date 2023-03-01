6cbbcf0f136bd584f127e4392a67fa06f33fc222
xpetersen <ptrsnxndr@gmail.com>
2023-03-01 14:49:09 +0100

    camera opens

--- native-app/App.js
+++ native-app/App.js
@@ -1,13 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera } from 'expo-camera'
+import { StatusBar } from 'expo-status-bar'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [startCamera, setStartCamera] = useState(false)
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [capturedImage, setCapturedImage] = useState(null)
+
+  let camera = Camera;
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true);
+    } else {
+      Alert.alert('Access denied');
+    }
+  };
+
+  const CameraPreview = ({photo}) => {
+    console.log('sdsfds', photo);
+    return (
+      <View
+        style={{
+          backgroundColor: 'transparent',
+          flex: 1,
+          width: '100%',
+          height: '100%'
+        }}
+      >
+        <ImageBackground
+          source={{uri: photo && photo.uri}}
+          style={{
+            flex: 1
+          }}
+        />
+      </View>
+    );
+  };
+  
+
+
+  if (startCamera) {
+    return (
+      <Camera
+        style={{flex: 1, width: "100%"}}
+        ref={(r) => {
+          camera = r
+        }}
+      ></Camera>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+          onPress={__startCamera}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: '#14274e',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 40
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +95,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
