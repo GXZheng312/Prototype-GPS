6776e3d421bd26b9e9fc42ad234d72f3524eb80d
FedorTselishchev <ikbenfedor@gmail.com>
2023-03-02 14:35:23 +0100

    Revert "inb"
    This reverts commit 490f0bb7e279c06e2c17ac4f5e9e7a9af8ef2b30.

--- moesland/App.js
+++ moesland/App.js
@@ -1,39 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
-
-state = {
-    location: {},
-    errorMessage: ''
-}
-
-
-
-_getLocation =  async() => {
-  const {status} = Permissions.askAsync(Permissions.Location);
-
-  if (status !== 'granted')
-  {
-      console.log('GPS PERMISSION NOT GRANTED!');
-      this.setState({
-          errorMessage: 'GPS PERMISSION IS NOT GRANTED!'
-        })
-  }
-  const userLocation = await Location.getCurrentPositionAsync();
-  this._getLocation;//
-  this.setState({
-    location
-  })
-}
-
-
 export default function App() {
-  //https://github.com/michalchudziak/react-native-geolocation
   return (
     <View style={styles.container}>
-      <Text>{JSON.stringify(this.state.location)} </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
