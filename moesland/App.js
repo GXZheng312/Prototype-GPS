8b5ca44f18e4c6a36a45fb813a7c2ac2644f83fe
FedorTselishchev <ikbenfedor@gmail.com>
2023-03-02 14:35:03 +0100

    fix?

--- moesland/App.js
+++ moesland/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
+import { Permissions, Location } from 'expo';
+
 
 state = {
     location: {},
