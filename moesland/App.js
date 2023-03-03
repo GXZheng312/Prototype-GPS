4b53fd95e360ea09a43324adb4ca5ace45abce64
FedorTselishchev <ikbenfedor@gmail.com>
2023-03-03 15:28:50 +0100

    cleanclean

--- moesland/App.js
+++ moesland/App.js
@@ -26,7 +26,7 @@ export default function App() {
         setLocationAccuracy(currentLocation.coords.accuracy);
       }
       else if(!status.granted){
-        alert("The permission to use geolocation has not been grated");
+        alert("The permission to use geolocation has not been granted");
       }
 
     };
@@ -35,10 +35,7 @@ export default function App() {
   }, []);
 
   const fetchData = async () => {
-    //faking fetching data from them backend
-    let moeslandLatitude = 37.4226711;
     setMoeslandLatitude(37.41)
-    let moeslandLongitude = -122.0849872;
     setMoeslandLongitude(-122.06)
     let moeslandRange = 1000.00;
     setMoeslandRange(moeslandRange)
@@ -49,8 +46,8 @@ export default function App() {
     let status = await Location.requestForegroundPermissionsAsync();
     if(status.granted){
 
-      const earthRadius = 6371e3; // metres
-      const φ1 = moeslandLatitude * Math.PI/180; // φ, λ in radians
+      const earthRadius = 6371e3; // meters
+      const φ1 = moeslandLatitude * Math.PI/180; //in radians
       const φ2 = locationLatitude * Math.PI/180;
       const Δφ = (locationLatitude-moeslandLatitude) * Math.PI/180;
       const Δλ = (locationLongitude-moeslandLongitude) * Math.PI/180;
@@ -59,11 +56,10 @@ export default function App() {
                 Math.cos(φ1) * Math.cos(φ2) *
                 Math.sin(Δλ/2) * Math.sin(Δλ/2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      const rangeBetweenTwoPoints = earthRadius * c; // in metres
-      console.log(rangeBetweenTwoPoints);
+      const rangeBetweenTwoPoints = earthRadius * c; 
       if(rangeBetweenTwoPoints >= parseFloat(moeslandRange + locationAccuracy))
       {
-        alert("you are outside te range to vote, distance in meters is: " + rangeBetweenTwoPoints + "/n range is: " + moeslandRange)
+        alert("you are outside the range to vote, distance in meters is: " + rangeBetweenTwoPoints + "\n range is: " + moeslandRange)
       }
       else{
         alert("you have succesfully voted for your favorite team!")
@@ -71,7 +67,7 @@ export default function App() {
     }
 
   };
-
+  
   const vote = async () => {
     fetchData();
     checkIfInRange();
