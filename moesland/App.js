fda458520bcfd1d47dc5c8895ea105d259e54245
FedorTselishchev <ikbenfedor@gmail.com>
2023-03-03 14:50:04 +0100

    finish full functionality

--- moesland/App.js
+++ moesland/App.js
@@ -6,7 +6,10 @@ import * as Location from 'expo-location';
 
 
 export default function App() {
-  const [location, setLocation] = useState();
+  const [locationLatitude, setLocationLatitude] = useState();
+  const [locationLongitude, setLocationLongitude] = useState();
+  const [locationAccuracy, setLocationAccuracy] = useState();
+
   const [moeslandLatitude, setMoeslandLatitude] = useState();
   const [moeslandLongitude, setMoeslandLongitude] = useState();
   const [moeslandRange, setMoeslandRange] = useState();
@@ -15,17 +18,14 @@ export default function App() {
   useEffect(() => {
     const getPermissions = async () => {
       let status = await Location.requestForegroundPermissionsAsync();
-      console.log("Status is:");
-      console.log(status.granted);
 
       if(status.granted){
-        console.log("Status is granted!");
         let currentLocation = await Location.getCurrentPositionAsync({}); 
-        console.log(location);
-        setLocation(currentLocation);
+        setLocationLatitude(currentLocation.coords.latitude);
+        setLocationLongitude(currentLocation.coords.longitude);
+        setLocationAccuracy(currentLocation.coords.accuracy);
       }
       else if(!status.granted){
-        console.log("The permission to use geolocation has not been grated");
         alert("The permission to use geolocation has not been grated");
       }
 
@@ -37,30 +37,41 @@ export default function App() {
   const fetchData = async () => {
     //faking fetching data from them backend
     let moeslandLatitude = 37.4226711;
-    setMoeslandLatitude(37.4226711)
-    console.log(moeslandLatitude);
+    setMoeslandLatitude(37.41)
 
     let moeslandLongitude = -122.0849872;
-    setMoeslandLongitude(-122.0849872)
-    console.log(moeslandLongitude);
+    setMoeslandLongitude(-122.06)
 
-    let moeslandRange = 1000;
+    let moeslandRange = 1000.00;
     setMoeslandRange(moeslandRange)
-    console.log(moeslandRange);
 
   };
 
   const checkIfInRange = async () => {
-    
-    if()
+    let status = await Location.requestForegroundPermissionsAsync();
+    if(status.granted){
 
-    if(true)
-    {
-      alert("you have succesfully voted for your favorite team!")
+      const earthRadius = 6371e3; // metres
+      const φ1 = moeslandLatitude * Math.PI/180; // φ, λ in radians
+      const φ2 = locationLatitude * Math.PI/180;
+      const Δφ = (locationLatitude-moeslandLatitude) * Math.PI/180;
+      const Δλ = (locationLongitude-moeslandLongitude) * Math.PI/180;
+
+      const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      const rangeBetweenTwoPoints = earthRadius * c; // in metres
+      console.log(rangeBetweenTwoPoints);
+      if(rangeBetweenTwoPoints >= parseFloat(moeslandRange + locationAccuracy))
+      {
+        alert("you are outside te range to vote, distance in meters is: " + rangeBetweenTwoPoints + "/n range is: " + moeslandRange)
+      }
+      else{
+        alert("you have succesfully voted for your favorite team!")
+      }
     }
-    else{
-      alert("you are outside te range to vote")
-    }
+
   };
 
   const vote = async () => {
