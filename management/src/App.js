4e91592cf5058f3f7e01c76e3b1e7f3c0c628481
Sven <sven.de.bresser@hotmail.com>
2023-03-03 08:59:02 +0100

    Bootstrap added

--- management/src/App.js
+++ management/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Form() {
   const [longitude, setLongitude] = useState('');
@@ -8,18 +9,26 @@ function Form() {
   const handleSubmit = useSubmitForm(longitude, latitude, range);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="longitude">Longitude</label>
-      <input type="text" id="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+    <div class="container">
+      <h3>Locatie toevoegen aan MongoDB</h3>
+      <form onSubmit={handleSubmit}>
+        <div class="form-group col-4">
+          <label htmlFor="longitude">Longitude</label>
+          <input class="form-control" type="text" id="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+        </div>
+        <div class="form-group col-4">
+          <label htmlFor="latitude">Latitude</label>
+          <input class="form-control" type="text" id="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+        </div>
+        <div class="form-group col-3">
+          <label htmlFor="range">Range</label>
+          <input class="form-control" type="text" id="range" value={range} onChange={(e) => setRange(e.target.value)} />
 
-      <label htmlFor="latitude">Latitude</label>
-      <input type="text" id="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
-
-      <label htmlFor="range">Range</label>
-      <input type="text" id="range" value={range} onChange={(e) => setRange(e.target.value)} />
-
-      <button type="submit">Submit</button>
-    </form>
+        </div>
+        <br></br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   );
 }
 
